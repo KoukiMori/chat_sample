140441e539e85f3db4239642aea06dc17ea15d87
koukimori <jieihal090310@gmail.com>
2022-09-13 13:09:16 +0900

    first commitgit add .

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_sample/post.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'firebase_options.dart';
+import 'package:flutter_chat_sample/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // runApp 前に何かを実行したいときはこれが必要です。
+  await Firebase.initializeApp(
+    // これが Firebase の初期化処理です。
+    options: DefaultFirebaseOptions.android,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignInPage(),
+    );
+  }
+}
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  Future<void> signInWithGoogle() async {
+    final googleUser =
+        await GoogleSignIn(scopes: ['profile', 'email']).signIn();
+    final googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GoogleSignIn'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('GoogleSignIn'),
+          onPressed: () async {
+            await signInWithGoogle();
+            print(FirebaseAuth.instance.currentUser?.displayName);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+final postsReference =
+    FirebaseFirestore.instance.collection('posts').withConverter<Post>(
+  fromFirestore: (documentSnapshot, _) {
+    return Post.fromFirestore(documentSnapshot);
+  },
+  toFirestore: (data, _) {
+    return data.toMap();
+  },
+);
